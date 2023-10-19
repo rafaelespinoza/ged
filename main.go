c08ab7f986df5233c5d2def9caf53c374df9857b
Rafael Espinoza <rafael@rafaelespinoza.com>
2023-10-19 11:36:04 -0700

    cmd: add simple CLI to test out gedcom conversions

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/rafaelespinoza/reltree/internal/log"
+	"github.com/rafaelespinoza/reltree/internal/srv"
+)
+
+func init() {
+	replaceAttrs := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey {
+			return slog.Attr{}
+		}
+		return a
+	}
+
+	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo, ReplaceAttr: replaceAttrs})
+	log.Init(h)
+}
+
+func main() {
+	people, unions, err := srv.ParseGedcom(context.Background(), os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	writeJSON(os.Stdout, map[string]any{"people": people, "unions": unions})
+}
+
+func writeJSON(out io.Writer, data any) error { return json.NewEncoder(out).Encode(data) }
