767e0781439243b8bc797f6e264d83487fc624db
Rafael Espinoza <rafael@rafaelespinoza.com>
2023-10-22 12:29:21 -0700

    refactor(cmd): rm unnecessary flag for parse

--- internal/cmd/parse.go
+++ internal/cmd/parse.go
@@ -11,15 +11,11 @@ import (
 )
 
 func makeParse(name string) alf.Directive {
-	var inputFormat string
-	supportedInputFormats := []string{"gedcom"}
-
 	out := alf.Command{
 		Description: "read GEDCOM data from STDIN, transform to entity types, write to STDOUT",
 		Setup: func(_ flag.FlagSet) *flag.FlagSet {
 			fullName := mainName + " " + name
 			flags := newFlagSet(fullName)
-			flags.StringVar(&inputFormat, "f", "gedcom", fmt.Sprintf("input format, one of %q", supportedInputFormats))
 
 			flags.Usage = func() {
 				fmt.Fprintf(flags.Output(), `%s < path/to/input
@@ -30,29 +26,19 @@ Description:
 	This subcommand is for meant for inspecting the data transformations applied
 	to the input data.
 
-	Supported input formats:
-		%q
-
 	The output shape:
 		{
 		  "people": []entity.Person{}.
 		  "unions": []entity.Union{},
 		}
 `,
-					initUsageLine(name), supportedInputFormats,
+					initUsageLine(name),
 				)
 				printFlagDefaults(flags)
 			}
 			return flags
 		},
 		Run: func(ctx context.Context) error {
-			switch inputFormat {
-			case supportedInputFormats[0]:
-				break
-			default:
-				return fmt.Errorf("invalid input format (flag -f) %q", inputFormat)
-			}
-
 			people, unions, err := srv.ParseGedcom(context.Background(), os.Stdin)
 			if err != nil {
 				return err
